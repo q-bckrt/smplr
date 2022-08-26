1408f50a89a72941fead063bd246b58314d06207
q-bckrt <pom.schc@gmail.com>
2022-08-26 19:13:15 +0200

    added some comments to clarify engine.js

--- engine.js
+++ engine.js
@@ -1,4 +1,9 @@
-function playSample(e) {
+//called when a key with a corresponding pad is pressed
+//1.play the sample
+//2.display sample name on lcd screen and highlight the pad in red
+//3.remove the red highlight when the key is unpressed.
+
+function keyOn(e) {
     const sample = document.querySelector(`audio[data-key="${e.keyCode}"]`);
     if (!sample) return;
     sample.currentTime = 0;
@@ -10,13 +15,15 @@ function playSample(e) {
     display.textContent = pad.id;
 }
 
-function removeRed(e) {
+function keyOff(e) {
     const pad = document.querySelector(`.pad[data-key="${e.keyCode}"]`)
     pad.classList.remove('push_btn');
 }
 
 window.addEventListener("keydown", playSample);
 window.addEventListener("keyup", removeRed);
+
+//when the mouse over on pad, show momentarily its name on the lcd screen.
 
 function showSmpOnHover (e) {
     const display = document.querySelector('.lcd_text');
